a20b240aeb3f1ace64a9e3b18946eac8dd2213a5
Luis Molina <luis_03_06_2@hotmail.com>
2021-12-07 04:11:57 -0300

    Ya tiene boostrap, header y footer - clase19

---
+++ ProyectoCoder/ProyectoCoder/AppCoder/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from AppCoder import views
+
+urlpatterns = [
+    
+    path('inicio', views.inicio),
+    
+    
+    
+]
--- ProyectoCoder/ProyectoCoder/AppCoder/views.py
+++ ProyectoCoder/ProyectoCoder/AppCoder/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from django.shortcuts import HttpResponse
+
 # Create your views here.
+
+#Primer Vista
+def inicio(request):
+    
+    #return HttpResponse("Esto de es una prueba del inicio")
+    
+    return render(request, 'AppCoder/inicio.html')
