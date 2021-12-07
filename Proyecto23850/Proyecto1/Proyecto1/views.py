f4df14de0f3863b2baf14cd459583ff8dae730da
Luis Molina <luis_03_06_2@hotmail.com>
2021-12-07 02:16:26 -0300

    Clase Intermedia Parte 1

--- Proyecto23850/Proyecto1/Proyecto1/views.py
+++ Proyecto23850/Proyecto1/Proyecto1/views.py
@@ -2,6 +2,9 @@ from django.http import HttpResponse
 from datetime import datetime
 from django.template import Template, Context
 
+#Paso 1
+from django.template import loader
+
 def saludo(request):
     return HttpResponse("Soy Luis - Hola Django - Coder")
 
@@ -23,15 +26,30 @@ def apellido(request,ape):
     return HttpResponse(f"El profe de coder {ape}, es muy bueno..<br><br>..- Por lo menos hoy:{fecha}")
 
 def probandoTemplate(request):
+    
+    mejorEstudiante = "Ilan Fritzler"
+    
+    nota = 8.9
+    
+    fecha = datetime.now()
+    
+    estudianteMasSimpaticos = ["Juanse","Nadia","Cristo","Laura"]
+    
+    dicc = {"nombre":mejorEstudiante, "nota":nota, "fecha":fecha, "estudiantes":estudianteMasSimpaticos}
+    
+    #Paso 3
+    plantilla = loader.get_template("template1.html")
 
-    miHTML = open("C:/Users/luis_/Desktop/Proyecto23850/Proyecto1/Proyecto1/plantillas/template1.html")
+    #miHTML = open("C:/Users/luis_/Desktop/Proyecto23850/Proyecto1/Proyecto1/plantillas/template1.html")
 
-    plantilla = Template(miHTML.read())
+    #plantilla = Template(miHTML.read())
 
-    miHTML.close()
+    #miHTML.close()
 
-    miCOntexto = Context()
+    #miCOntexto = Context(dicc)
 
-    documento = plantilla.render(miCOntexto)
+    #documento = plantilla.render(miCOntexto)
+
+    documento = plantilla.render(dicc)
 
     return HttpResponse(documento)
